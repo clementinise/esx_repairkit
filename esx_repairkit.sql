6a39a541aded6f42dbd60ce0b77c8dc6d7f36bc4
Demo <51279632+Demo4889@users.noreply.github.com>
2020-05-04 21:30:48 -0400

    Update esx_repairkit.sql
    Updated SQL for body kit addition and new weight for ES_Extended

--- esx_repairkit.sql
+++ esx_repairkit.sql
@@ -1,4 +1,5 @@
-INSERT INTO items (`name`,`label`,`limit`,`rare`,`can_remove`) VALUES
-('repairkit', 'Repairkit', 3, 0, 1),
-('tyrekit', 'Tyrekit', 3, 0, 1)
+INSERT INTO items (`name`,`label`,`weight`,`rare`,`can_remove`) VALUES
+('repairkit', 'Repair Kit', 5, 0, 1),
+('tyrekit', 'Tire kit', 5, 0, 1),
+('bodykit', 'Body Kit', 5, 0, 1)
 ;
