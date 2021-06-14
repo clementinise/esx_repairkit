89d65922a02ea02844307761b68e1b8ddab0a195
X00LA <X00LA@users.noreply.github.com>
2021-06-14 20:06:47 +0200

    Renamed tyrekit to spare wheel

--- esx_repairkit.sql
+++ esx_repairkit.sql
@@ -1,3 +1,3 @@
 INSERT INTO items (`name`,`label`,`weight`,`rare`,`can_remove`) VALUES
 ('repairkit', 'Repairkit', 1000, 0, 1),
-('tyrekit', 'Tyrekit', 3500, 0, 1);
+('tyrekit', 'Spare Wheel', 3500, 0, 1);
