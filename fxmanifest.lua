247caf1aa6d4522ae261bd898239d0f47e44bb9d
X00LA <X00LA@users.noreply.github.com>
2021-06-14 19:44:59 +0200

    Created fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'jonteohr'
+description 'ESX Repairkit'
+version '3.1'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'client/main.lua',
+	'locales/fr.lua',
+	'locales/en.lua',
+	'locales/sv.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'server/main.lua',
+	'locales/fr.lua',
+	'locales/en.lua',
+	'locales/sv.lua',
+	'config.lua'
+}
