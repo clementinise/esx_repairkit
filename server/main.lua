8085054cf60a8b0cb0304c04bf09b51e497d8bf9
Markus Petautschnig <xoolalp@gmail.com>
2021-06-20 09:26:13 +0200

    Fix upload

--- server/main.lua
+++ server/main.lua
@@ -2,24 +2,30 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 -- Update Checker
-local CurrentVersion = '3.5' -- Do Not Change This Value
+local CurrentVersion = '3.6' -- Do Not Change This Value
 
 PerformHttpRequest('https://raw.githubusercontent.com/X00LA/esx_repairkit/master/version', function(Error, NewestVersion, Header)
-		print('\n')
-		print('########################')
-		print('## RepairKit Resource')
-		print('##')
-		print('## Current Version: ' .. CurrentVersion)
-		print('## Newest Version: ' .. NewestVersion)
-		print('##')
+			print('\n')
+			print('^8-------------------------------------------------------')
+			print('^5ESX ^2RepairKit')
+			print(' ')
+			print('^5Current Version: ^0' .. CurrentVersion)
+			print('^5Newest Version: ^0' .. NewestVersion)
+			print('^5Changelog: ')
+			print('^0- Fixed \'Not near engine\' error and make repairkit usable again.')
+			print('^0- Updated sql for compatibility with ESX 1.2+.')
+			print('^0- Added german language.')
+			print('^0- Renamed Tyrekit to spare wheel. (makes more sense)')
+			print('^0- Fixed outdated \'Mecano\' calls to actual \'Mechanic\'. (Doesnt fix problem with mechanics using repairkit.)')
+			print(' ')
 		if CurrentVersion == NewestVersion then
-			print('## Up to date!')
-			print('########################')
+			print('^2 You are up to date!')
+			print('^8-------------------------------------------------------')
 		else
-			print('## Outdated')
-			print('## Please check the GitHub and download the last update')
-			print('## https://github.com/X00LA/esx_repairkit/releases/latest')
-			print('########################')
+			print('^1 Your version of ^5ESX ^2Repairkit ^1is Outdated!')
+			print('^0 Please check the GitHub and download the last update')
+			print('^0 https://github.com/X00LA/esx_repairkit/releases/latest')
+			print('^8-------------------------------------------------------')
 		end
 		print('\n')
 end)
@@ -29,10 +35,10 @@ ESX.RegisterUsableItem('repairkit', function(source)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	
-	if Config.AllowMecano then
+	if Config.AllowMechanic then
 		TriggerClientEvent('esx_repairkit:onUse', _source)
 	else
-		if xPlayer.job.name ~= 'mecano' then
+		if xPlayer.job.name ~= 'mechanic' then
 			TriggerClientEvent('esx_repairkit:onUse', _source)
 		end
 	end
@@ -42,10 +48,10 @@ ESX.RegisterUsableItem('tyrekit', function(source)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	
-	if Config.AllowMecano then
+	if Config.AllowMechanic then
 		TriggerClientEvent('tyrekit:onUse', _source)
 	else
-		if xPlayer.job.name ~= 'mecano' then
+		if xPlayer.job.name ~= 'mechanic' then
 			TriggerClientEvent('tyrekit:onUse', _source)
 		end
 	end
@@ -73,7 +79,7 @@ AddEventHandler('esx_repairkit:removeKit', function()
 	end
 end)
 
-function MecanoOnline()
+function MechanicOnline()
 
 	local xPlayers = ESX.GetPlayers()
 
@@ -81,22 +87,22 @@ function MecanoOnline()
 
 	for i=1, #xPlayers, 1 do
 		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-		if xPlayer.job.name == 'mecano' then
+		if xPlayer.job.name == 'mechanic' then
 			MechanicConnected = MechanicConnected + 1
 		end
 	end
 
-	SetTimeout(10000, MecanoOnline)
+	SetTimeout(10000, MechanicOnline)
 
 end
 
-MecanoOnline()
+MechanicOnline()
 
 RegisterServerEvent('jobonline:get')
 AddEventHandler('jobonline:get', function()
 	local counted = {}
 
-	counted['mecano'] = MechanicConnected
+	counted['mechanic'] = MechanicConnected
 
 	TriggerClientEvent('jobonline:set', source, counted)
 end)
